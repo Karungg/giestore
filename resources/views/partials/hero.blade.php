d3ae4edd4c8fcb30597ca4a8461cb211019e27b2
Karungg <miftahfadilah71@gmail.com>
2024-03-17 15:15:13 +0700

    slicing template

---
+++ resources/views/partials/hero.blade.php
@@ -0,0 +1,11 @@
+<section id="hero">
+    <div class="hero min-h-80">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-4xl text-secondary font-bold">Selamat Datang di Giestore</h1>
+                <p class="py-6">destinasi utama bagi para pencinta coklat dan pecinta cemilan. Kami menyajikan
+                    rangkaian produk pilihan yang memikat hati dan memanjakan lidah.</p>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,9 @@
+<header class="border-b border-palette-lighter sticky top-0 z-20 bg-white">
+    <div class="flex items-center justify-between mx-auto max-w-6xl px-6 pb-2 pt-4 md:pt-6"><a class=" cursor-pointer"
+            href="/">
+            <h1 class="flex no-underline"><img height="32" width="32" alt="logo"
+                    class="h-8 w-14 mr-1 object-contain" src="{{ asset('assets/img/logo.png') }}" /><span
+                    class="text-2xl font-primary text-secondary font-bold tracking-tight pt-1">Giestore</span></h1>
+        </a>
+    </div>
+</header>
